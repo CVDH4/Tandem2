cfebcf27d03ace9b77a33ace2ccbc9e9ad727a3a
CVDH4 <cvitale@gradcenter.cuny.edu>
2016-10-05 21:54:26 -0400

    Housekeeping / Adding Semantic UI

--- server/main.js
+++ server/main.js
@@ -7,12 +7,3 @@ Meteor.startup(function () {
     checkCreateDirectories: true //create the directories for you
   });
 });
-
-Meteor.startup(() => {
-  // code to run on server at startup
-  // UploadServer.init({
-  //   tmpDir: process.env.PWD + '/.uploads/tmp',
-  //   uploadDir: process.env.PWD + '/.uploads/',
-  //   checkCreateDirectories: true //create the directories for you
-  // });
-});
