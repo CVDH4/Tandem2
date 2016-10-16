fe9fa1dfdc8eaed20dcd84bc5351cec3f854c1bb
CVDH4 <cvitale@gradcenter.cuny.edu>
2016-10-15 23:17:11 -0400

    OCR Experimenting

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
+import 'node-tesseract';
 import './main.html';
 
 Resolutions = new Mongo.Collection('resolutions');
@@ -13,4 +13,32 @@ Meteor.startup(function() {
   Uploader.uploadUrl = Meteor.absoluteUrl("Upload");
 });
 
-// Experimenting with image stuff
+// Experimenting with OCR
+
+var tesseract = require('node-tesseract');
+ 
+// Recognize text of any language in any format
+
+tesseract.process(__dirname + '/.uploads/AlphabetBook_Page_09.jpg',function(err, text) {
+    if(err) {
+        console.error(err);
+    } else {
+        console.log(text);
+    }
+});
+ 
+// Recognize German text in a single uniform block of text and set the binary path
+ 
+// var options = {
+//     l: 'deu',
+//     psm: 6,
+//     binary: '/usr/local/bin/tesseract'
+// };
+ 
+// tesseract.process(__dirname + '/path/to/image.jpg', options, function(err, text) {
+//     if(err) {
+//         console.error(err);
+//     } else {
+//         console.log(text);
+//     }
+// });
