95f628df5f66e15090d027c0b8babe69d1a22296
CVDH4 <cvitale@gradcenter.cuny.edu>
2016-12-14 22:16:18 -0500

    Pulling the RGB values of an image on the DOM

--- client/main.js
+++ client/main.js
@@ -68,30 +68,25 @@ Template.uploadedInfo.events({
 
 Meteor.startup(function() {
   var img = new Image();
-  img.src = '../images/placekitten.jpeg';
+  img.src = '../images/placekitten.jpg';
 
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
   var imageData = img.data;
-  var pixels = (0, 0, 10000, 10000);
+  var pixels = (0, 0, 600, 600);
   var r, g, b, a;
 
   img.onload = function() {
     ctx.drawImage(img, 0, 0);
-    ctx.createImageData(10000, 10000);
+    ctx.createImageData(600, 600);
     img.style.display = 'none';
-
-    console.log(img.src);
-    console.log("Height = " + img.height);
-    console.log("Width = " + img.width);
-    console.log("Total Pixels = " + img.width * img.height);
 
     document.getElementById("stats").innerHTML = (img.src);
     document.getElementById("imageHeight").innerHTML = (img.height);
     document.getElementById("imageWidth").innerHTML = (img.width);
     document.getElementById("imageTotalPixels").innerHTML = (img.width * img.height);
 
-    while(--pixels){
+    while(pixels){
        imageData[4*pixels+0] = r; // Red value
        imageData[4*pixels+1] = g; // Green value
        imageData[4*pixels+2] = b; // Blue value
@@ -108,13 +103,8 @@ document.body.onload = addElement;
 
   function addElement () { 
     var newDiv = document.createElement("div"); 
-    var newContent = document.createTextNode("Hi there and greetings!");
-      // document.createTextNode("Height = " + img.height);
-      // document.createTextNode("Width = " + img.width);
-      // document.createTextNode("Total Pixels = " + img.width * img.height);
+    var newContent = document.createTextNode("Images:");
     newDiv.appendChild(newContent); 
-    //add the text node to the newly created div. 
-    // add the newly created element and its content into the DOM 
     var currentDiv = document.getElementById("div1"); 
     document.body.insertBefore(newDiv, currentDiv); 
   };
@@ -132,4 +122,74 @@ document.body.onload = addElement;
     }
     canvas.addEventListener('mousemove', pick);
 
+});
+
+// TESTING PIXEL ITERATION - http://stackoverflow.com/questions/21176827/js-generate-rgb-of-each-pixel-in-an-image
+// CODE ADAPTED FROM - http://www.html5canvastutorials.com/advanced/html5-canvas-get-image-data-tutorial/
+
+function drawImage(imageObj) {
+
+    var codepanel = document.getElementById('code');
+    var canvas = document.getElementById('myCanvas');
+    var context = canvas.getContext('2d');
+    var imageX = 69;
+    var imageY = 50;
+    var imageWidth = imageObj.width;
+    var imageHeight = imageObj.height;
+    var pixels = new Array();
+    var pixel = 0; 
+
+    context.drawImage(imageObj, imageX, imageY);
+
+    var imageData = context.getImageData(imageX, imageY, imageWidth, imageHeight);
+    var data = imageData.data;
+
+    // iterate over all pixels
+    for(var i = 0, n = data.length; i < n; i += 4) {
+      var red = data[i];
+      var green = data[i + 1];
+      var blue = data[i + 2];
+      var alpha = data[i + 3];
+       pixels[pixel] = red + " " + green + " " + blue + " ";
+       pixel++; 
+    }
+
+    codepanel.innerHTML =  pixels.join(); //my addition
+
+    var x = 20;
+    var y = 20;
+    var red = data[((imageWidth * y) + x) * 4];
+    var green = data[((imageWidth * y) + x) * 4 + 1];
+    var blue = data[((imageWidth * y) + x) * 4 + 2];
+    var alpha = data[((imageWidth * y) + x) * 4 + 3];
+
+    for(var y = 0; y < imageHeight; y++) {
+      for(var x = 0; x < imageWidth; x++) {
+        var red = data[((imageWidth * y) + x) * 4];
+        var green = data[((imageWidth * y) + x) * 4 + 1];
+        var blue = data[((imageWidth * y) + x) * 4 + 2];
+        var alpha = data[((imageWidth * y) + x) * 4 + 3];         
+      }
+    }
+  }
+
+  var imageObj = new Image();
+  imageObj.onload = function() {
+    drawImage(this);
+  };
+  imageObj.src = '../images/placekitten.jpg';
+
+  var csvContent = "data:text/csv;charset=utf-8,";
+    pixels.forEach(function(infoArray, index){
+
+    dataString = infoArray.join(",");
+    csvContent += index < pixels.length ? dataString+ "\n" : dataString;
+
+    var encodedUri = encodeURI(csvContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "my_data.csv");
+    document.body.appendChild(link); // Required for FF
+
+    link.click(); // This will download the data file named "my_data.csv".
 });
